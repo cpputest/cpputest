1fe280ec80e144084dcf2e60967ae9bd03fbeba8
Bas Vodde <basv@odd-e.com>
2007-08-30 08:46:05 +0000

    Added some methods because of symbian port

--- src/Platforms/Gcc/GccMemoryLeakWarning.cpp
+++ src/Platforms/Gcc/GccMemoryLeakWarning.cpp
@@ -102,6 +102,27 @@ void reportMemoryBallance()
 
 MemoryLeakWarning* MemoryLeakWarning::_latest = NULL;
 
+MemoryLeakWarning::MemoryLeakWarning()
+{
+	_latest = this; 
+	CreateData();	
+}
+
+MemoryLeakWarning::~MemoryLeakWarning()
+{
+	DestroyData();
+}
+
+MemoryLeakWarning* MemoryLeakWarning::GetLatest()
+{
+	return _latest;
+}
+
+void MemoryLeakWarning::SetLatest(MemoryLeakWarning* latest)
+{
+	_latest = latest;
+}
+
 void MemoryLeakWarning::Enable()
 {
   _impl->initialBlocksUsed = allocatedBlocks;
