b793a16cdc58a3af55744a0b37ccda48e0ba5563
Bas Vodde <basv@odd-e.com>
2009-07-26 07:05:39 +0000

    Removed include

--- tests/Extensions/SimpleStringExtensionsTest.cpp
+++ tests/Extensions/SimpleStringExtensionsTest.cpp
@@ -25,7 +25,6 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <string>
 #include "CppUTest/Extensions/SimpleStringExtensions.h"
 #include "CppUTest/TestHarness.h"
 
