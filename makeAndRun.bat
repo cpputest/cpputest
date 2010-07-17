55c84e6493e58169086932bf46212d72d5241b28
James Grenning <james@grenning.net>
2010-07-17 19:54:17 +0000

    added command line build for vc6.  found an easy way.  I updated my vc6 to sp6 to make it work.

--- makeAndRun.bat
+++ makeAndRun.bat
@@ -1,16 +1,17 @@
 rem ****
-rem * for this command line build to work independent of where it was created
+rem * Command line build - For CppUTest - Run from dsw directory
 rem * 
-rem * Generate Project/Export Makefiles
-rem *
-rem * then do these edits to the generated files
-rem * CppUTest.dep - change the relitive path to ....\program files to \program files
-rem * AllTests.mak - At the end of the makefile where the depended upon CppUTest is made
-rem*       Change cd "\absolute\path\CppUTest" to cd ..
-PATH=C:\Program Files\Microsoft Visual Studio\VC98\Bin;%PATH%
-rem nmake /f CppUTest.mak CFG="CppUTest - Win32 Debug" all
-cd tests
-nmake /f AllTests.mak all
-cd Debug
-AllTests -v
-cd ..\..
+rem * A single parameter is suported and it is the last parameter of msdev
+rem * for example:
+rem * 	/clean
+rem * make sure to use the the slash
+
+rem * this needs to be in your path 
+rem ****PATH=C:\Program Files\Microsoft Visual Studio\VC98\Bin;%PATH%
+
+msdev CppUTest.dsp /MAKE "CppUTest - Debug" %1
+msdev tests\AllTests.dsp /MAKE "AllTests - Debug" %1
+if "%1" EQU "/clean" goto end
+if "%1" EQU "/CLEAN" goto end
+tests\Debug\AllTests -v
+:end
