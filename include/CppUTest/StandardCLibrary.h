c369cc535f6c7b1be8a75ece32df038ad83cf849
Bas Vodde <basv@odd-e.com>
2011-08-08 08:55:52 +0000

    The missing file

---
+++ include/CppUTest/StandardCLibrary.h
@@ -0,0 +1,14 @@
+
+#ifndef STANDARDCLIBRARY_H_
+#define STANDARDCLIBRARY_H_
+
+/* Needed for size_t */
+#include <stddef.h>
+
+/* Needed for malloc */
+#include <stdlib.h>
+
+/* Needed for ... */
+#include <stdarg.h>
+
+#endif
