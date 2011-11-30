772caefa4d9c9cfddfb60779412c8e8ae43e828c
James Grenning <james@grenning.net>
2011-11-30 04:32:21 +0000

    trying out PlatformSpecificFunctions_c.h

---
+++ include/CppUTest/PlatformSpecificFunctions_c.h
@@ -0,0 +1,16 @@
+/*
+ * PlatformSpecificFunctions_c.h
+ *
+ *  Created on: Nov 29, 2011
+ *      Author: james
+ */
+
+#ifndef PLATFORMSPECIFICFUNCTIONS_C_H_
+#define PLATFORMSPECIFICFUNCTIONS_C_H_
+
+
+int PlatformSpecificVSNprintf(char *str, unsigned int size, const char* format,
+		va_list va_args_list);
+
+
+#endif /* PLATFORMSPECIFICFUNCTIONS_C_H_ */
