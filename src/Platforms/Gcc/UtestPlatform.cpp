dfebed0e4a74838187d68033f3f158ea4ede8733
Brett N. DiFrischia <orderthruchaos@gmail.com>
2012-10-18 14:09:07 -0500

    Move 'extern "C"' functions into 'extern "C"' blocks.

--- src/Platforms/Gcc/UtestPlatform.cpp
+++ src/Platforms/Gcc/UtestPlatform.cpp
@@ -51,27 +51,36 @@
 static jmp_buf test_exit_jmp_buf[10];
 static int jmp_buf_index = 0;
 
-int PlatformSpecificSetJmp(void (*function) (void* data), void* data)
-{
-	if (0 == setjmp(test_exit_jmp_buf[jmp_buf_index])) {
-	    jmp_buf_index++;
-		function(data);
-	    jmp_buf_index--;
-		return 1;
-	}
-	return 0;
-}
+extern "C" {
 
-void PlatformSpecificLongJmp()
-{
-	jmp_buf_index--;
-	longjmp(test_exit_jmp_buf[jmp_buf_index], 1);
-}
 
-void PlatformSpecificRestoreJumpBuffer()
-{
-	jmp_buf_index--;
-}
+    int PlatformSpecificSetJmp(void (*function) (void* data), void* data)
+    {
+        if (0 == setjmp(test_exit_jmp_buf[jmp_buf_index])) {
+            jmp_buf_index++;
+            function(data);
+            jmp_buf_index--;
+            return 1;
+        }
+        return 0;
+    }
+
+    void PlatformSpecificLongJmp()
+    {
+        jmp_buf_index--;
+        longjmp(test_exit_jmp_buf[jmp_buf_index], 1);
+    }
+
+    void PlatformSpecificRestoreJumpBuffer()
+    {
+        jmp_buf_index--;
+    }
+
+
+} // extern "C"
+
+
+
 
 void PlatformSpecificRunTestInASeperateProcess(UtestShell* shell, TestPlugin* plugin, TestResult* result)
 {
@@ -97,6 +106,9 @@ void PlatformSpecificRunTestInASeperateProcess(UtestShell* shell, TestPlugin* pl
 #endif
 }
 
+
+
+
 TestOutput::WorkingEnvironment PlatformSpecificGetWorkingEnvironment()
 {
 	return TestOutput::eclipse;
@@ -114,15 +126,32 @@ static long TimeInMillisImplementation()
 
 static long (*timeInMillisFp) () = TimeInMillisImplementation;
 
-long GetPlatformSpecificTimeInMillis()
-{
-	return timeInMillisFp();
-}
 
-void SetPlatformSpecificTimeInMillisMethod(long (*platformSpecific) ())
-{
-	timeInMillisFp = (platformSpecific == 0) ? TimeInMillisImplementation : platformSpecific;
-}
+
+
+extern "C" {
+
+
+    long GetPlatformSpecificTimeInMillis()
+    {
+        return timeInMillisFp();
+    }
+
+    void SetPlatformSpecificTimeInMillisMethod(long (*platformSpecific) ())
+    {
+        if (platformSpecific == 0) {
+            timeInMillisFp = TimeInMillisImplementation;
+        } else {
+            timeInMillisFp = platformSpecific;
+        }
+        // timeInMillisFp = (platformSpecific == 0) ? TimeInMillisImplementation : platformSpecific;
+    }
+
+
+} // extern "C"
+
+
+
 
 ///////////// Time in String
 
@@ -139,118 +168,132 @@ const char* GetPlatformSpecificTimeString()
 	return timeStringFp();
 }
 
-void SetPlatformSpecificTimeStringMethod(const char* (*platformMethod) ())
-{
-	timeStringFp = (platformMethod == 0) ? TimeStringImplementation : platformMethod;
-}
-
-int PlatformSpecificAtoI(const char*str)
-{
-   return atoi(str);
-}
-
-size_t PlatformSpecificStrLen(const char* str)
-{
-   return strlen(str);
-}
-
-char* PlatformSpecificStrCat(char* s1, const char* s2)
-{
-   return strcat(s1, s2);
-}
-
-char* PlatformSpecificStrCpy(char* s1, const char* s2)
-{
-   return strcpy(s1, s2);
-}
-
-char* PlatformSpecificStrNCpy(char* s1, const char* s2, size_t size)
-{
-   return strncpy(s1, s2, size);
-}
-
-int PlatformSpecificStrCmp(const char* s1, const char* s2)
-{
-   return strcmp(s1, s2);
-}
-
-int PlatformSpecificStrNCmp(const char* s1, const char* s2, size_t size)
-{
-   return strncmp(s1, s2, size);
-}
-char* PlatformSpecificStrStr(const char* s1, const char* s2)
-{
-   return (char*) strstr(s1, s2);
-}
-
-int PlatformSpecificVSNprintf(char *str, size_t size, const char* format, va_list args)
-{
-   return vsnprintf( str, size, format, args);
-}
-
-char PlatformSpecificToLower(char c)
-{
-	return (char) tolower((char) c);
-}
-
-PlatformSpecificFile PlatformSpecificFOpen(const char* filename, const char* flag)
-{
-   return fopen(filename, flag);
-}
 
 
-void PlatformSpecificFPuts(const char* str, PlatformSpecificFile file)
-{
-   fputs(str, (FILE*)file);
-}
 
-void PlatformSpecificFClose(PlatformSpecificFile file)
-{
-   fclose((FILE*)file);
-}
-
-void PlatformSpecificFlush()
-{
-  fflush(stdout);
-}
-
-int PlatformSpecificPutchar(int c)
-{
-  return putchar(c);
-}
-
-void* PlatformSpecificMalloc(size_t size)
-{
-   return malloc(size);
-}
-
-void* PlatformSpecificRealloc (void* memory, size_t size)
-{
-   return realloc(memory, size);
-}
-
-void PlatformSpecificFree(void* memory)
-{
-   free(memory);
-}
-
-void* PlatformSpecificMemCpy(void* s1, const void* s2, size_t size)
-{
-   return memcpy(s1, s2, size);
-}
-
-void* PlatformSpecificMemset(void* mem, int c, size_t size)
-{
-	return memset(mem, c, size);
-}
+extern "C" {
 
 
-double PlatformSpecificFabs(double d)
-{
-   return fabs(d);
-}
+    void SetPlatformSpecificTimeStringMethod(const char* (*platformMethod) ())
+    {
+        if (platformMethod == 0) {
+            timeStringFp = TimeStringImplementation;
+        } else {
+            timeStringFp = platformMethod;
+        }
+        // timeStringFp = (platformMethod == 0) ? TimeStringImplementation : platformMethod;
+    }
 
-int PlatformSpecificIsNan(double d)
-{
-	return isnan((float)d);
-}
+    int PlatformSpecificAtoI(const char*str)
+    {
+        return atoi(str);
+    }
+
+    size_t PlatformSpecificStrLen(const char* str)
+    {
+        return strlen(str);
+    }
+
+    char* PlatformSpecificStrCat(char* s1, const char* s2)
+    {
+        return strcat(s1, s2);
+    }
+
+    char* PlatformSpecificStrCpy(char* s1, const char* s2)
+    {
+        return strcpy(s1, s2);
+    }
+
+    char* PlatformSpecificStrNCpy(char* s1, const char* s2, size_t size)
+    {
+        return strncpy(s1, s2, size);
+    }
+
+    int PlatformSpecificStrCmp(const char* s1, const char* s2)
+    {
+        return strcmp(s1, s2);
+    }
+
+    int PlatformSpecificStrNCmp(const char* s1, const char* s2, size_t size)
+    {
+        return strncmp(s1, s2, size);
+    }
+    char* PlatformSpecificStrStr(const char* s1, const char* s2)
+    {
+        return (char*) strstr(s1, s2);
+    }
+
+    int PlatformSpecificVSNprintf(char *str, size_t size, const char* format, va_list args)
+    {
+        return vsnprintf( str, size, format, args);
+    }
+
+    char PlatformSpecificToLower(char c)
+    {
+        return (char) tolower((char) c);
+    }
+
+    PlatformSpecificFile PlatformSpecificFOpen(const char* filename, const char* flag)
+    {
+        return fopen(filename, flag);
+    }
+
+
+    void PlatformSpecificFPuts(const char* str, PlatformSpecificFile file)
+    {
+        fputs(str, (FILE*)file);
+    }
+
+    void PlatformSpecificFClose(PlatformSpecificFile file)
+    {
+        fclose((FILE*)file);
+    }
+
+    void PlatformSpecificFlush()
+    {
+        fflush(stdout);
+    }
+
+    int PlatformSpecificPutchar(int c)
+    {
+        return putchar(c);
+    }
+
+    void* PlatformSpecificMalloc(size_t size)
+    {
+        return malloc(size);
+    }
+
+    void* PlatformSpecificRealloc (void* memory, size_t size)
+    {
+        return realloc(memory, size);
+    }
+
+    void PlatformSpecificFree(void* memory)
+    {
+        free(memory);
+    }
+
+    void* PlatformSpecificMemCpy(void* s1, const void* s2, size_t size)
+    {
+        return memcpy(s1, s2, size);
+    }
+
+    void* PlatformSpecificMemset(void* mem, int c, size_t size)
+    {
+        return memset(mem, c, size);
+    }
+
+
+    double PlatformSpecificFabs(double d)
+    {
+        return fabs(d);
+    }
+
+    int PlatformSpecificIsNan(double d)
+    {
+        return isnan((float)d);
+    }
+
+
+} // extern "C"
