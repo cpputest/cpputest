c64727aa425fc1465162b870fe2d8cba9bb8a06e
yocchi <yocchiman@gmail.com>
2013-04-10 20:12:13 +0900

    Added .travis.yml

--- cmake/Modules/CppUTestWarningFlags.cmake
+++ cmake/Modules/CppUTestWarningFlags.cmake
@@ -5,6 +5,24 @@ else (MSVC)
     include(CheckCCompilerFlag)
     include(CheckCXXCompilerFlag)
 
+    macro(check_and_append_c_warning_flags)
+      foreach (flag ${ARGN})
+        check_c_compiler_flag("-${flag}" WARNING_C_FLAG_${flag})
+        if (WARNING_C_FLAG_${flag})
+            set(CPPUTEST_C_WARNING_FLAGS "${CPPUTEST_C_WARNING_FLAGS} -${flag}")
+        endif (WARNING_C_FLAG_${flag})
+      endforeach (flag)
+    endmacro(check_and_append_c_warning_flags)
+
+    macro(check_and_append_cxx_warning_flags)
+      foreach (flag ${ARGN})
+        check_c_compiler_flag("-${flag}" WARNING_CXX_FLAG_${flag})
+        if (WARNING_CXX_FLAG_${flag})
+            set(CPPUTEST_CXX_WARNING_FLAGS "${CPPUTEST_CXX_WARNING_FLAGS} -${flag}")
+        endif (WARNING_CXX_FLAG_${flag})
+      endforeach (flag)
+    endmacro(check_and_append_cxx_warning_flags)
+
     set(WARNING_C_FLAGS
         Werror
         Weverything
@@ -18,49 +36,22 @@ else (MSVC)
         Wsign-conversion
         Woverloaded-virtual
         Wno-padded
+        Wno-disabled-macro-expansion
         )
 
     set(WARNING_C_ONLY_FLAGS
         Wstrict-prototypes
         )
 
-    set(WARNING_CXX_FLAGS
+    set(WARNING_CXX_ONLY_FLAGS
         Wno-global-constructors
         Wno-exit-time-destructors
         Wno-weak-vtables
         )
 
-    foreach (flag ${WARNING_C_FLAGS})
-        check_c_compiler_flag("-${flag}" WARNING_FLAG_${flag})
-        if (WARNING_FLAG_${flag})
-            set(CPPUTEST_C_WARNING_FLAGS "${CPPUTEST_C_WARNING_FLAGS} -${flag}")
-        endif (WARNING_FLAG_${flag})
-    endforeach (flag ${WARNING_C_FLAGS})
-    check_c_compiler_flag(-Wno-disabled-macro-expansion WARNING_FLAG_no_disabled_macro_expansion)
-    if (WARNING_FLAG_no_disabled_macro_expansion)
-        set(CPPUTEST_C_WARNING_FLAGS "${CPPUTEST_C_WARNING_FLAGS} -Wno-disabled-macro-expansion")
-    endif (WARNING_FLAG_no_disabled_macro_expansion)
+    check_and_append_c_warning_flags(${WARNING_C_FLAGS})
+    check_and_append_c_warning_flags(${WARNING_C_ONLY_FLAGS})
+    check_and_append_cxx_warning_flags(${WARNING_C_FLAGS})
+    check_and_append_cxx_warning_flags(${WARNING_CXX_ONLY_FLAGS})
 
-    set(CPPUTEST_CXX_WARNING_FLAGS "${CPPUTEST_CXX_WARNING_FLAGS} ${CPPUTEST_C_WARNING_FLAGS}")
-    check_cxx_compiler_flag(-Wno-global-constructors WARNING_FLAG_no_global_constructors)
-    if (WARNING_FLAG_no_global_constructors)
-        set(CPPUTEST_CXX_WARNING_FLAGS "${CPPUTEST_CXX_WARNING_FLAGS} -Wno-global-constructors")
-    endif (WARNING_FLAG_no_global_constructors)
-
-    check_cxx_compiler_flag(-Wno-exit-time-destructors WARNING_FLAG_no_exit_time_destructors)
-    if (WARNING_FLAG_no_exit_time_destructors)
-        set(CPPUTEST_CXX_WARNING_FLAGS "${CPPUTEST_CXX_WARNING_FLAGS} -Wno-exit-time-destructors")
-    endif (WARNING_FLAG_no_exit_time_destructors)
-
-    check_cxx_compiler_flag(-Wno-weak-vtables WARNING_FLAG_no_weak_vtables)
-    if (WARNING_FLAG_no_weak_vtables)
-        set(CPPUTEST_CXX_WARNING_FLAGS "${CPPUTEST_CXX_WARNING_FLAGS} -Wno-weak-vtables")
-    endif (WARNING_FLAG_no_weak_vtables)
-
-    foreach (flag ${WARNING_C_ONLY_FLAGS})
-        check_c_compiler_flag("-${flag}" WARNING_FLAG_${flag})
-        if (WARNING_FLAG_${flag})
-            set(CPPUTEST_C_WARNING_FLAGS "${CPPUTEST_C_WARNING_FLAGS} -${flag}")
-        endif (WARNING_FLAG_${flag})
-    endforeach (flag ${WARNING_C_ONLY_FLAGS})
 endif (MSVC)
