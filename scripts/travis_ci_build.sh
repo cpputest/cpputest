7fb019cb5c6b2cc0ebb00f9697a60726655dd30a
Bas Vodde <basv@odd-e.com>
2013-06-14 09:43:50 +0800

    Hopefully now it will fail properly

--- scripts/travis_ci_build.sh
+++ scripts/travis_ci_build.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 # Script run in the travis CI
 
-if [ $BUILDTOOL = "autotools" ]; then ../configure && echo "CONFIGURATION DONE. Compiling now." && make check_all; if [ $CXX = "gcc" ]; then echo "Release now"; fi; fi
-if [ $BUILDTOOL = "cmake" ]; then cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE && make && ctest -V; fi
-if [ $BUILDTOOL = "cmake" ]; then cmake .. -DGMOCK=ON && make && ctest -V; fi
-if [ $BUILDTOOL = "cmake" ]; then cmake .. -DGMOCK=OFF -DREAL_GTEST=ON && make && ctest -V; fi
+if [ "x$BUILDTOOL" = "xautotools" ]; then 
+	../configure && echo "CONFIGURATION DONE. Compiling now." && 
+	make check_all && 
+	if [ $CXX = "gcc" ]; then 
+		echo "Release now"; 
+	fi; 
+fi
+
+if [ "x$BUILDTOOL" = "cmake" ]; then 
+	cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE && 
+	make && 
+	ctest -V; 
+fi
+
+if [ "x$BUILDTOOL" = "cmake" ]; then 
+	cmake .. -DGMOCK=ON && 
+	make && 
+	ctest -V; 
+fi
+
+if [ "x$BUILDTOOL" = "cmake" ]; then 
+	cmake .. -DGMOCK=OFF -DREAL_GTEST=ON && 
+	make && 
+	ctest -V; 
+fi
