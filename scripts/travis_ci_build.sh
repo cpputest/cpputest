cbd3188c6145959943f9de767ed039548d3ce983
Bas Vodde <basv@odd-e.com>
2013-06-14 13:53:44 +0800

    Trying to get the fucking script to fail

--- scripts/travis_ci_build.sh
+++ scripts/travis_ci_build.sh
@@ -2,27 +2,29 @@
 # Script run in the travis CI
 
 if [ "x$BUILDTOOL" = "xautotools" ]; then 
-	../configure && echo "CONFIGURATION DONE. Compiling now." && 
-	make check_all && 
+	../configure || exit 1
+	echo "CONFIGURATION DONE. Compiling now."
+	make check_all || exit 1
+	i
 	if [ $CXX = "gcc" ]; then 
 		echo "Release now"; 
 	fi; 
 fi
 
 if [ "x$BUILDTOOL" = "cmake" ]; then 
-	cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE && 
-	make && 
-	ctest -V; 
+	cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE || exit 1
+	make || exit 1
+	ctest -V || exit 1
 fi
 
 if [ "x$BUILDTOOL" = "cmake" ]; then 
-	cmake .. -DGMOCK=ON && 
-	make && 
-	ctest -V; 
+	cmake .. -DGMOCK=ON || exit 1
+	make || exit 1
+	ctest -V || exit 1
 fi
 
 if [ "x$BUILDTOOL" = "cmake" ]; then 
-	cmake .. -DGMOCK=OFF -DREAL_GTEST=ON && 
-	make && 
-	ctest -V; 
+	cmake .. -DGMOCK=OFF -DREAL_GTEST=ON || exit 1
+	make || exit 1
+	ctest -V || exit 1
 fi
