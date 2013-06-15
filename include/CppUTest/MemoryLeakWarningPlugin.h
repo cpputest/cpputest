1b91a6e2804af08ae9d86e846755ba0b5c4e82ed
Bas Vodde <basv@odd-e.com>
2013-06-15 19:41:49 +0800

    gcc warning removed

--- include/CppUTest/MemoryLeakWarningPlugin.h
+++ include/CppUTest/MemoryLeakWarningPlugin.h
@@ -43,10 +43,10 @@
 	void* operator new[](size_t size, const std::nothrow_t&) UT_THROW( );
 #endif
 
-void operator delete(void* mem) UT_THROW( );
-void operator delete[](void* mem) UT_THROW( );
-void operator delete(void* mem, const char* file, int line) UT_THROW( );
-void operator delete[](void* mem, const char* file, int line) UT_THROW( );
+void operator delete(void* mem) UT_THROW(/*none*/);
+void operator delete[](void* mem) UT_THROW(/*none*/);
+void operator delete(void* mem, const char* file, int line) UT_THROW(/*none*/);
+void operator delete[](void* mem, const char* file, int line) UT_THROW(/*none*/);
 
 #endif
 
