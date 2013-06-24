cdbaf5088c2edc9498ea812b27f2099050eafe37
Arnd Strube <a.strube@wind-soft.de>
2013-06-24 21:18:43 +0200

    Explicit cast to void* in StringformatFromPointer.

--- tests/SimpleStringTest.cpp
+++ tests/SimpleStringTest.cpp
@@ -440,7 +440,7 @@ TEST(SimpleString, StringFromFormat)
 TEST(SimpleString, StringFromFormatpointer)
 {
 	//this is not a great test. but %p is odd on mingw and even more odd on Solaris.
-	SimpleString h1 = StringFromFormat("%p", 1);
+	SimpleString h1 = StringFromFormat("%p", (void*) 1);
 	if (h1.size() == 3)
 		STRCMP_EQUAL("0x1", h1.asCharString())
 	else if (h1.size() == 8)
