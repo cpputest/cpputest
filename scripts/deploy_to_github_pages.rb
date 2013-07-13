6abb4a66d17f69b554bc80e783d42c14609c6ef5
Bas Vodde <basv@odd-e.com>
2013-07-13 14:59:46 +0800

    Setting the password too

--- scripts/deploy_to_github_pages.rb
+++ scripts/deploy_to_github_pages.rb
@@ -5,20 +5,32 @@ class GithubPagesDeployerForCppUTest
   def self.push_artifacts
     deployer = GithubPagesDeployerForCppUTest.new
     deployer.clone_cpputest_pages
-    deployer.set_credentials
+    deployer.prepare_credentials_based_on_environment_variables
   end
 
   def clone_cpputest_pages
     system("git clone https://github.com/cpputest/cpputest.github.io.git github_pages")
   end
   
-  def set_credentials
-    username = ENV['GIT_NAME']
-    raise StandardError.new("The GIT_NAME environment variable wasn't set.") if username.empty?
-    system("git config user.name '#{username}'")
-
-    
+  def prepare_credentials_based_on_environment_variables
+    set_username_based_on_environment_variable
+    set_email_based_on_environment_variable
   end
+  
+  def set_username_based_on_environment_variable
+    git_config_based_on_enviroment_variable("user.name", "GIT_NAME")
+  end
+  
+  def set_email_based_on_environment_variable
+    git_config_based_on_enviroment_variable("user.email", "GIT_EMAIL")
+  end
+  
+  def git_config_based_on_enviroment_variable(config_parameter, environment_variable)
+    config_value = ENV[environment_variable]
+    raise StandardError.new("The #{environment_variable} environment variable wasn't set.") if config_value.nil?
+    system("git config #{config_parameter} '#{config_value}'")
+  end
+  
   
 end
 
--- scripts/deploy_to_github_pages_spec.rb
+++ scripts/deploy_to_github_pages_spec.rb
@@ -9,21 +9,34 @@ describe "configuring the git environment for deploying to github pages" do
     subject.should_receive(:system).with("git clone https://github.com/cpputest/cpputest.github.io.git github_pages")
     subject.clone_cpputest_pages
   end
+
+  it "Should be able to set the credentials for pushing stuff up" do
+    subject.should_receive(:set_username_based_on_environment_variable)
+    subject.should_receive(:set_email_based_on_environment_variable)
+    subject.prepare_credentials_based_on_environment_variables
+  end
   
-  it "Should be able to set the username, email and credentials in order to push" do
+  it "Should be able to set the username based on an environment variable" do
     ENV['GIT_NAME'] = "basvodde"
     subject.should_receive(:system).with("git config user.name 'basvodde'")
-    subject.set_credentials    
+    subject.set_username_based_on_environment_variable    
   end
   
   it "Should give an error message when the GIT_NAME isn't set" do
-    ENV['GIT_NAME'] = ""
-    expect {subject.set_credentials}.to raise_error(StandardError, "The GIT_NAME environment variable wasn't set.")
+    ENV['GIT_NAME'] = nil
+    expect {subject.set_username_based_on_environment_variable}.to raise_error(StandardError, "The GIT_NAME environment variable wasn't set.")
+  end
+
+  it "Should be able to set the password based on an environment variable" do
+    ENV['GIT_EMAIL'] = "basv@bestcompanythatexists.com"
+    subject.should_receive(:system).with("git config user.email 'basv@bestcompanythatexists.com'")
+    subject.set_email_based_on_environment_variable
+    
   end
   
   it "should push the right artifacts up to github" do
     subject.should_receive(:clone_cpputest_pages)
-    subject.should_receive(:set_credentials)
+    subject.should_receive(:prepare_credentials_based_on_environment_variables)
     GithubPagesDeployerForCppUTest.should_receive(:new).and_return(subject)
     GithubPagesDeployerForCppUTest.push_artifacts
   end
