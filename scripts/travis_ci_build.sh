2b698277f174f546a0f67a264df4ea17fa84e34b
basvodde <basv@bestcompanythatexists.com>
2013-07-17 20:41:25 +0800

    Added generation of junit output to the build script

--- scripts/travis_ci_build.sh
+++ scripts/travis_ci_build.sh
@@ -4,12 +4,19 @@
 if [ "x$BUILDTOOL" = "xautotools" ]; then 
 	../configure || exit 1
 	echo "CONFIGURATION DONE. Compiling now."
-	make check_all || exit 1
+#	make check_all || exit 1
+
+	# One more normal build for generating test reports
+	../configure || exit 1
+	make check
+	cp ../scripts/generate_junit_report_ant.xml .
+	ant -f generate_junit_report_ant.xml
+
 	if [ "x$CXX" = "xg++" ]; then 
 		date >> ../README
 		cd .. && travis_github_deployer -v || exit 1
 	fi; 
-fi
+#fi
 
 if [ "x$BUILDTOOL" = "xcmake" ]; then 
 	cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE || exit 1
