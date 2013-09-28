2f0626507555e51d0119d38fc20e14b94e5ebf56
Martin Ertsaas <martiert@gmail.com>
2013-09-28 13:12:59 +0200

    travis: Compile with C++11 support with cmake.

--- scripts/travis_ci_build.sh
+++ scripts/travis_ci_build.sh
@@ -2,6 +2,9 @@
 # Script run in the travis CI
 
 if [ $BUILDTOOL = "autotools" ]; then ../configure && echo "CONFIGURATION DONE. Compiling now." && make check_all; if [ $CXX = "gcc" ]; then echo "Release now"; fi; fi
-if [ $BUILDTOOL = "cmake" ]; then cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE && make && ctest -V; fi
+if [ $BUILDTOOL = "cmake" ]; then cmake .. && make && ctest -V; fi
+if [ $BUILDTOOL = "cmake" ]; then cmake .. -DC++11=ON && make && ctest -V; fi
 if [ $BUILDTOOL = "cmake" ]; then cmake .. -DGMOCK=ON && make && ctest -V; fi
+if [ $BUILDTOOL = "cmake" ]; then cmake .. -DGMOCK=ON -DC++11=ON && make && ctest -V; fi
 if [ $BUILDTOOL = "cmake" ]; then cmake .. -DGMOCK=OFF -DREAL_GTEST=ON && make && ctest -V; fi
+if [ $BUILDTOOL = "cmake" ]; then cmake .. -DGMOCK=OFF -DREAL_GTEST=ON -DC++11=ON && make && ctest -V; fi
