ce06e3ddd6531455ce9f4d45fc4bf7a801ea9eeb
Arnd R. Strube <a.strube@wind-soft.de>
2014-03-19 13:45:08 +0100

    Step 5.1: Remove unwanted operations from MockActualFunctionCall
    (this is what we set out to do!)

--- src/CppUTestExt/MockActualFunctionCall.cpp
+++ src/CppUTestExt/MockActualFunctionCall.cpp
@@ -243,42 +243,6 @@ void CheckedMockActualFunctionCall::setState(ActualCallState state)
 	state_ = state;
 }
 
-MockActualFunctionCall& CheckedMockActualFunctionCall::andReturnValue(unsigned int)
-{
-	FAIL("andReturnValue cannot be called on an ActualFunctionCall. Use returnValue instead to get the value.");
-	return *this;
-}
-
-MockActualFunctionCall& CheckedMockActualFunctionCall::andReturnValue(int)
-{
-	FAIL("andReturnValue cannot be called on an ActualFunctionCall. Use returnValue instead to get the value.");
-	return *this;
-}
-
-MockActualFunctionCall& CheckedMockActualFunctionCall::andReturnValue(const char*)
-{
-	FAIL("andReturnValue cannot be called on an ActualFunctionCall. Use returnValue instead to get the value.");
-	return *this;
-}
-
-MockActualFunctionCall& CheckedMockActualFunctionCall::andReturnValue(double)
-{
-	FAIL("andReturnValue cannot be called on an ActualFunctionCall. Use returnValue instead to get the value.");
-	return *this;
-}
-
-MockActualFunctionCall& CheckedMockActualFunctionCall::andReturnValue(void*)
-{
-	FAIL("andReturnValue cannot be called on an ActualFunctionCall. Use returnValue instead to get the value.");
-	return *this;
-}
-
-MockActualFunctionCall& CheckedMockActualFunctionCall::andReturnValue(const void*)
-{
-	FAIL("andReturnValue cannot be called on an ActualFunctionCall. Use returnValue instead to get the value.");
-	return *this;
-}
-
 MockNamedValue CheckedMockActualFunctionCall::returnValue()
 {
 	checkExpectations();
