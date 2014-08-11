141dfdaebf6a1e8a20dc5287c822add66a31b2b1
Arnd R. Strube <a.strube@wind-soft.de>
2014-08-11 08:23:23 +0200

    Simpler and more like StrCmp()

--- src/CppUTest/SimpleString.cpp
+++ src/CppUTest/SimpleString.cpp
@@ -95,9 +95,8 @@ size_t SimpleString::StrLen(const char* str)
 
 int SimpleString::StrNCmp(const char* s1, const char* s2, size_t n)
 {
-    while (n && *s1 && *s1 == *s2) {
+    while (n > 1 && *s1 && *s1 == *s2) {
         n--, s1++, s2++;
-        if (n == 0) return 0;
     }
     return *(unsigned char *) s1 - *(unsigned char *) s2;
 }
