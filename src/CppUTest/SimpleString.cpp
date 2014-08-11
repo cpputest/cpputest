2ae4d7515b86a388e53889248ec4102967ed2a53
Arnd R. Strube <a.strube@wind-soft.de>
2014-08-11 07:48:43 +0200

    @terryyin's solution

--- src/CppUTest/SimpleString.cpp
+++ src/CppUTest/SimpleString.cpp
@@ -81,13 +81,9 @@ int SimpleString::AtoI(const char* str)
 
 int SimpleString::StrCmp(const char* s1, const char* s2)
 {
-    while (true) {
-        if (*s1 != *s2)
-            return *(unsigned char *) s1 - *(unsigned char *) s2;
-        if (*s1 == 0)
-            return 0;
-        s1++, s2++;
-    }
+   while(*s1 && *s1 == *s2)
+      s1++, s2++;
+   return *(unsigned char *) s1 - *(unsigned char *) s2;
 }
 
 size_t SimpleString::StrLen(const char* str)
