29b619b7726112c0b5465a27711c7d5874c05c78
Arnd R. Strube <a.strube@wind-soft.de>
2014-08-12 09:32:16 +0200

    Try memset()

--- src/Platforms/GccNoStdC/UtestPlatform.cpp
+++ src/Platforms/GccNoStdC/UtestPlatform.cpp
@@ -31,8 +31,15 @@
 #undef free
 
 extern "C" {
-    void * vmalloc(size_t);
-    void vfree(void *);
+    
+/// #include "linux/vmalloc.h"    
+void * vmalloc(unsigned long);                           
+void vfree(void *);
+                                
+/// #include "linux/string.h"
+typedef unsigned int	__kernel_size_t;
+extern void * memset(void *, int, __kernel_size_t);
+
 }
 
 #include "CppUTest/PlatformSpecificFunctions.h"
@@ -132,9 +139,8 @@ void* PlatformSpecificMalloc(size_t size)
 
 void* PlatformSpecificRealloc (void* memory, size_t size)
 {
-    /* To be implemented */
-    (void) memory;
-    (void) size;
+    (void)memory;
+    (void)size;
     return NULL;
 }
 
@@ -154,11 +160,7 @@ void* PlatformSpecificMemCpy(void* s1, const void* s2, size_t size)
 
 void* PlatformSpecificMemset(void* mem, int c, size_t size)
 {
-    /* To be implemented */
-    (void) mem;
-    (void) c;
-    (void) size;
-    return NULL;
+   return memset(mem, c, size);
 }
 
 double PlatformSpecificFabs(double d)
