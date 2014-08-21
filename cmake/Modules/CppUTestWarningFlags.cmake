c6c696fdc296f36d5155c44abeb46d1d1a13303a
Filipe Alves <filipe.alves@bithium.com>
2014-08-21 22:52:39 +0100

    Fix CMake variable usage.

--- cmake/Modules/CppUTestWarningFlags.cmake
+++ cmake/Modules/CppUTestWarningFlags.cmake
@@ -56,7 +56,7 @@ else (MSVC)
     check_and_append_c_warning_flags(${WARNING_C_ONLY_FLAGS})
     check_and_append_cxx_warning_flags(${WARNING_CXX_FLAGS})
 
-    if (C++11)
+    if (CPPUTEST_C++11)
         check_cxx_compiler_flag("-Wno-c++98-compat" NO_WARNING_CXX_98_COMPAT_FLAG)
         if (NO_WARNING_CXX_98_COMPAT_FLAG)
             set(CPPUTEST_CXX_WARNING_FLAGS "${CPPUTEST_CXX_WARNING_FLAGS} -Wno-c++98-compat")
