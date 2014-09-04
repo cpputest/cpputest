9932f6b67eba4d73c78bf3084c24df2cf5097448
bortsov <bortsov@yandex.ru>
2014-09-04 10:08:48 +0700

    Adding some documentaion and LICENSE to AT91SAM7A3 port

--- platforms_examples/armcc/AT91SAM7A3/tests/main.cpp
+++ platforms_examples/armcc/AT91SAM7A3/tests/main.cpp
@@ -17,11 +17,9 @@
 //- ------------------------------------------------------------------
 
 #include "CppUTest/CommandLineTestRunner.h"
-#include <iostream>
 
 int main(int ac, char** av)
 {
-    std::cout << "Hello, World!\n";
     int result = RUN_ALL_TESTS(ac, av);
 
     return result;
