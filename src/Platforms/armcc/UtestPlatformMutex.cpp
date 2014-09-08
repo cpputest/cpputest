0958271e95c376b55a99fc34ad57f365bf34ab9f
Chen YewMing <fusion82@gmail.com>
2014-09-08 14:31:42 +0800

    Add SimpleMutex class and platform specific mutex function
    Add SimpleMutex unit test

---
+++ src/Platforms/armcc/UtestPlatformMutex.cpp
@@ -0,0 +1,23 @@
+#include "CppUTest/TestHarness.h"
+#include "CppUTest/PlatformSpecificFunctions.h"
+
+PlatformSpecificMutex PlatformSpecificMutexCreate(void)
+{
+    FAIL("PlatformSpecificMutexCreate is not implemented");
+}
+
+void PlatformSpecificMutexLock(PlatformSpecificMutex mtx)
+{
+    FAIL("PlatformSpecificMutexLock is not implemented");
+}
+
+void PlatformSpecificMutexUnlock(PlatformSpecificMutex mtx)
+{
+    FAIL("PlatformSpecificMutexUnlock is not implemented");
+}
+
+void PlatformSpecificMutexDestroy(PlatformSpecificMutex mtx)
+{
+    FAIL("PlatformSpecificMutexDestroy is not implemented");
+}
+
