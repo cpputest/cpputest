79e34e773011e8f57b6bf44801eca3e5a4350394
Arnd R. Strube <a.strube@wind-soft.de>
2015-01-16 08:11:44 +0100

    ...and again

--- makeVS201x.bat
+++ makeVS201x.bat
@@ -2,7 +2,7 @@ rem ****
 rem * Command line build - For CppUTest - Run from CppUTest directory
 rem * 
 rem * this path works on my machine
-rem **** PATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319;c:\windows\system32;c:\windows
+rem ****PATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319;c:\windows\system32;c:\windows
 msbuild /t:rebuild /verbosity:quiet CppUTest_VS201x.sln
 set test_exe=tests\Debug\AllTests.exe
 if exist %test_exe% %test_exe% -v
