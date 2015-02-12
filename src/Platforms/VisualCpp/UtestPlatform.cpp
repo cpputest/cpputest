0b0cbb1de679cfc8344231107ac12898dd919d5d
Paul Bußmann <paul@medwyn.de>
2015-02-12 12:01:48 +0100

    Revert "Fix VC98 compilation"
    This reverts commit a439126c33a999ddfbd0c0467e1f9b5e3a7c7361.

--- src/Platforms/VisualCpp/UtestPlatform.cpp
+++ src/Platforms/VisualCpp/UtestPlatform.cpp
@@ -73,7 +73,7 @@ static const char* TimeStringImplementation()
     time_t the_time = time(NULL);
     struct tm the_local_time;
     static char dateTime[80];
-    _getsystime(&the_local_time);
+    localtime_s(&the_local_time, &the_time);
     strftime(dateTime, 80, "%Y-%m-%dT%H:%M:%S", &the_local_time);
     return dateTime;
 }
@@ -87,7 +87,7 @@ int PlatformSpecificVSNprintf(char *str, size_t size, const char* format, va_lis
     char* buf = 0;
     size_t sizeGuess = size;
 
-    int result = _vsnprintf( str, size, format, args);
+    int result = _vsnprintf_s( str, size, _TRUNCATE, format, args);
     str[size-1] = 0;
     while (result == -1)
     {
@@ -95,7 +95,7 @@ int PlatformSpecificVSNprintf(char *str, size_t size, const char* format, va_lis
             free(buf);
         sizeGuess += 10;
         buf = (char*)malloc(sizeGuess);
-        result = _vsnprintf( buf, sizeGuess, format, args);
+        result = _vsnprintf_s( buf, sizeGuess, _TRUNCATE, format, args);
     }
 
     if (buf != 0)
@@ -106,7 +106,8 @@ int PlatformSpecificVSNprintf(char *str, size_t size, const char* format, va_lis
 
 PlatformSpecificFile PlatformSpecificFOpen(const char* filename, const char* flag)
 {
-   FILE* file = fopen(filename, flag);
+   FILE* file;
+   fopen_s(&file, filename, flag);
    return file;
 }
 
@@ -164,7 +165,7 @@ extern "C" int (*PlatformSpecificIsNan)(double) = _isnan;
 
 int PlatformSpecificVSNprintf(char *str, unsigned int size, const char* format, void* args)
 {
-   return _vsnprintf( str, size, format, (va_list) args);
+   return _vsnprintf_s( str, size, _TRUNCATE, format, (va_list) args);
 }
 
 static PlatformSpecificMutex Win32MutexCreate(void)
