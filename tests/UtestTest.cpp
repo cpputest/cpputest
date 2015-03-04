04d5c56f656c8f8d86e34facbdc8fc6fffa9f2c4
Arnd R. Strube <a.strube@wind-soft.de>
2015-03-04 09:11:52 +0100

    Add TEST(UtestShell, RunInSeparateProcessTest)
    Note: This test does not include platform specific code.

--- tests/UtestTest.cpp
+++ tests/UtestTest.cpp
@@ -161,6 +161,20 @@ TEST(UtestShell, this_test_covers_the_UtestShell_createTest_and_Utest_testBody_m
     LONGS_EQUAL(2, fixture.result_->getTestCount());
 }
 
+void StubPlatformSpecificRunTestInASeperateProcess(UtestShell* shell, TestPlugin* plugin, TestResult* result)
+{
+    shell->runOneTestInCurrentProcess(plugin, *result);
+    result->addFailure(TestFailure(shell, "Failed in separate process"));
+}
+
+TEST(UtestShell, RunInSeparateProcessTest)
+{
+    UT_PTR_SET(PlatformSpecificRunTestInASeperateProcess, StubPlatformSpecificRunTestInASeperateProcess);
+    fixture.registry_->setRunTestsInSeperateProcess();
+    fixture.runAllTests();
+    fixture.assertPrintContains("Failed in separate process");
+}
+
 #if CPPUTEST_USE_STD_CPP_LIB
 
 static bool destructorWasCalledOnFailedTest = false;
