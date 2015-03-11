a4906541adf13323088e92a34b14062979b5daad
Arnd R. Strube <a.strube@wind-soft.de>
2015-03-11 13:35:26 +0100

    Fix compilation issue with VS6:
    y:\github\paulbussmann\cpputest\src\cpputest\memoryleakdetector.cpp(81) : error C2374: 'p' : redefinition; multiple initialization
            y:\github\paulbussmann\cpputest\src\cpputest\memoryleakdetector.cpp(75) : see declaration of 'p'
    y:\github\paulbussmann\cpputest\src\cpputest\memoryleakdetector.cpp(89) : error C2374: 'p' : redefinition; multiple initialization
            y:\github\paulbussmann\cpputest\src\cpputest\memoryleakdetector.cpp(75) : see declaration of 'p'

--- src/CppUTest/MemoryLeakDetector.cpp
+++ src/CppUTest/MemoryLeakDetector.cpp
@@ -63,6 +63,7 @@ void SimpleStringBuffer::addMemoryDump(const void* memory, size_t memorySize)
     const unsigned char* byteMemory = (const unsigned char*)memory;
     const size_t maxLineBytes = 16;
     size_t currentPos = 0;
+	size_t p;
 
     while (currentPos < memorySize) {
         add("    %04lx: ", currentPos);
@@ -72,13 +73,13 @@ void SimpleStringBuffer::addMemoryDump(const void* memory, size_t memorySize)
         }
         const size_t leftoverBytes = maxLineBytes - bytesInLine;
 
-        for (size_t p = 0; p < bytesInLine; p++) {
+        for (p = 0; p < bytesInLine; p++) {
             add("%02hhx ", byteMemory[currentPos + p]);
             if (p == ((maxLineBytes / 2) - 1)) {
                 add(" ");
             }
         }
-        for (size_t p = 0; p < leftoverBytes; p++) {
+        for (p = 0; p < leftoverBytes; p++) {
             add("   ");
         }
         if (leftoverBytes > (maxLineBytes/2)) {
