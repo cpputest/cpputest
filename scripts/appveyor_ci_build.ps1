3eb63a481c46e54b8fc546c3841c94521d731e0d
Andy Neebel <andy@andne.net>
2015-04-12 17:00:57 -0500

    Added support for VS2010 as well

--- scripts/appveyor_ci_build.ps1
+++ scripts/appveyor_ci_build.ps1
@@ -9,15 +9,26 @@ function Get-Batchfile ($file) {
 }
 
 # The project files that will get built
-$VS2008ProjectFiles = @( 'CppUTest.vcproj', 'tests/AllTests.vcproj' )
+$VS2008ProjectFiles = @( 'CppUTest.vcproj' , 'tests/AllTests.vcproj'  )
+$VS2010ProjectFiles = @( 'CppUTest.vcxproj', 'tests/AllTests.vcxproj' )
 
-$vsvarspath = Join-Path $env:VS90COMNTOOLS vsvars32.bat
-Get-BatchFile($vsvarspath)
+if ($env:PlatformToolset -eq 'v90')
+{
+    $vsvarspath = Join-Path $env:VS90COMNTOOLS vsvars32.bat
+    Get-BatchFile($vsvarspath)
 
-$VS2008ProjectFiles | foreach {
-    vcbuild /upgrade $_
+    $VS2008ProjectFiles | foreach {
+        vcbuild /upgrade $_
+    }
+
+    $VS2008ProjectFiles | foreach {
+        vcbuild $_ Debug
+    }
 }
 
-$VS2008ProjectFiles | foreach {
-    vcbuild $_ Debug
+if ($env:PlatformToolset -eq 'v100')
+{
+    $VS2010ProjectFiles | foreach {
+        msbuild /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" $_
+    }
 }
