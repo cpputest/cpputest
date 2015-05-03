8e1dfc1b513658a9170d1e6b6c9bf7042dd61fae
Andy Neebel <andy@andne.net>
2015-05-03 16:27:18 -0500

    Need MinGW to be on the path when running tests

--- scripts/appveyor_ci_test.ps1
+++ scripts/appveyor_ci_test.ps1
@@ -37,12 +37,60 @@ function Invoke-Tests($executable)
     }
 }
 
+function Remove-PathFolder($folder)
+{
+    [System.Collections.ArrayList]$pathFolders = New-Object System.Collections.ArrayList
+    $env:Path -split ";" | foreach { $pathFolders.Add($_) | Out-Null }
+
+    for ([int]$i = 0; $i -lt $pathFolders.Count; $i++)
+    {
+        if ([string]::Compare($pathFolders[$i], $folder, $true) -eq 0)
+        {
+            Write-Host "Removing $folder from the PATH"
+            $pathFolders.RemoveAt($i)
+            $i--
+        }
+    }
+
+    $env:Path = $pathFolders -join ";"
+}
+
+function Add-PathFolder($folder)
+{
+    if (-not (Test-Path $folder))
+    {
+        Write-Host "Not adding $folder to the PATH, it does not exist"
+    }
+
+    [bool]$alreadyInPath = $false
+    [System.Collections.ArrayList]$pathFolders = New-Object System.Collections.ArrayList
+    $env:Path -split ";" | foreach { $pathFolders.Add($_) | Out-Null }
+
+    for ([int]$i = 0; $i -lt $pathFolders.Count; $i++)
+    {
+        if ([string]::Compare($pathFolders[$i], $folder, $true) -eq 0)
+        {
+            $alreadyInPath = $true
+            break
+        }
+    }
+
+    if (-not $alreadyInPath)
+    {
+        Write-Host "Adding $folder to the PATH"
+        $pathFolders.Insert(0, $folder)
+        $env:Path = $pathFolders -join ";"
+    }
+}
+
 if ($env:PlatformToolset -ne 'MinGW')
 {
     Invoke-Tests('.\cpputest_build\AllTests.exe')
 }
 else
 {
+    Add-PathFolder 'C:\Tools\mingw32\bin'
     Invoke-Tests('.\cpputest_build\tests\CppUTestTests.exe')
     Invoke-Tests('.\cpputest_build\tests\CppUTestExt\CppUTestExtTests.exe')
+    Remove-PathFolder 'C:\Tools\mingw32\bin'
 }
