c15c69f504e4959da6ab06f0e325d4b00721cd00
Bas Vodde <basv@odd-e.com>
2015-08-09 08:40:18 +0800

    Added additional stuff to the build matrix

--- scripts/travis_ci_build.sh
+++ scripts/travis_ci_build.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 # Script run in the travis CI
-set -e
+set -ex
 
-if [ "x$BUILDTOOL" = "xautotools" ]; then
+export CPPUTEST_BUILD_DIR=$TRAVIS_BUILD_DIR/cpputest_build
+mkdir -p $CPPUTEST_BUILD_DIR && cd $CPPUTEST_BUILD_DIR
+
+if [ "x$BUILD" = "xautotools" ]; then
     autoreconf -i ..
     ../configure
     echo "CONFIGURATION DONE. Compiling now."
     make check_all
-
-    # Special check for all the gtest versions.
-    make check_gtest
-
-    # One more normal build for generating test reports
-    ../configure
-    make check
-    ./CppUTestTests -ojunit
-    ./CppUTestExtTests -ojunit
-    cp ../scripts/generate_junit_report_ant.xml .
-    ant -f generate_junit_report_ant.xml
 
     make dist
     make dist-zip
@@ -28,17 +20,26 @@ if [ "x$BUILDTOOL" = "xautotools" ]; then
     fi;
 fi
 
-if [ "x$BUILDTOOL" = "xcmake" ]; then
+if [ "x$BUILD" = "xcmake" ]; then
     cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
-    make || exit 1
-    ctest -V || exit 1
+    make
+    ctest -V
 
     if [ "x$CXX" != "xg++" ]; then
         cmake .. -DC++11=ON -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
-        make || exit 1
-        ctest -V || exit 1
+        make
+        ctest -V
     fi
+fi
 
+if [ "x$BUILD" = "xautotools_gtest" ]; then
+    autoreconf -i ..
+    ../configure
+
+    make check_gtest
+fi
+
+if [ "x$BUILD" = "xcmake_gtest" ]; then
     wget https://googlemock.googlecode.com/files/gmock-1.6.0.zip
     unzip gmock-1.6.0.zip -d $TRAVIS_BUILD_DIR
     cd $TRAVIS_BUILD_DIR/gmock-1.6.0
@@ -47,20 +48,29 @@ if [ "x$BUILDTOOL" = "xcmake" ]; then
 
     export GMOCK_HOME=$TRAVIS_BUILD_DIR/gmock-1.6.0
     export GTEST_HOME=$TRAVIS_BUILD_DIR/gmock-1.6.0/gtest
-    cmake .. -DGMOCK=ON || exit 1
-    make || exit 1
-    ctest -V || exit 1
+    cmake .. -DGMOCK=ON
+    make
+    ctest -V
 fi
 
-if [ "x$BUILDTOOL" = "xcmake-coverage" ]; then
+if [ "x$BUILD" = "xtest_report" ]; then
+    ../configure
+    make check
+    ./CppUTestTests -ojunit
+    ./CppUTestExtTests -ojunit
+    cp ../scripts/generate_junit_report_ant.xml .
+    ant -f generate_junit_report_ant.xml
+fi
+
+if [ "x$BUILD" = "xcmake-coverage" ]; then
     cmake .. -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCOVERAGE=ON
-    make || exit 1
-    ctest || exit 1
+    make
+    ctest
 
-    coveralls -b . -r .. -i "src" -i "include" --gcov-options="-bc" || true
+    coveralls -b . -r .. -i "src" -i "include" --gcov-options="-bc"
 fi
 
-if [ "x$BUILDTOOL" = "xmake-dos" ]; then
+if [ "x$BUILD" = "xmake-dos" ]; then
     export CC=wcl
     export CXX=wcl
     # $(CC) --version
