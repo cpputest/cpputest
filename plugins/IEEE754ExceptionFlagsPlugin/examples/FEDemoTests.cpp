c6b3e25cb08d578d1bca011ff5f9db743c1bffdd
Arnd R. Strube <a.strube@wind-soft.de>
2015-08-13 16:07:01 +0200

    Add IEEE754ExceptionFlagsPlugin

---
+++ plugins/IEEE754ExceptionFlagsPlugin/examples/FEDemoTests.cpp
@@ -0,0 +1,37 @@
+#include "CppUTest/CommandLineTestRunner.h"
+#include "CppUTest/TestHarness.h"
+#include "CppUTest/TestRegistry.h"
+#include "IEEE754ExceptionFlagsPlugin.h"
+#include <cfenv>
+#include <limits>
+
+TEST_GROUP(FE_Demo) {
+};
+
+TEST(FE_Demo, should_fail_when__FE_DIVBYZERO__is_set) {
+    float f = 1.0f;
+    CHECK(f /= 0.0f == std::numeric_limits<float>::infinity() );
+}
+
+TEST(FE_Demo, should_fail_when__FE_UNDERFLOW__is_set) {
+    float f = 0.01f;
+    while (f > 0.0f) f *= f;
+    CHECK(f == 0.0f);
+}
+
+TEST(FE_Demo, should_fail_when__FE_OVERFLOW__is_set) {
+    float f = 1000.0f;
+    while (f < std::numeric_limits<float>::infinity()) f *= f;
+    CHECK(f == std::numeric_limits<float>::infinity());
+}
+
+TEST(FE_Demo, should_succeed_when_no_flags_are_set) {
+    CHECK(5.0f == 15.0f / 3.0f);
+}
+
+static IEEE754ExceptionFlagsPlugin ieee754Plugin{"IEEE754"};
+
+int main(int ac, char** av) {
+    TestRegistry::getCurrentRegistry()->installPlugin(&ieee754Plugin);
+	return RUN_ALL_TESTS(ac, av);
+}
