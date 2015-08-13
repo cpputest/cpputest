112868c345dcaa4a2ada6d4e822a58b5fc40dadc
Arnd R. Strube <a.strube@wind-soft.de>
2015-08-13 13:40:55 +0200

    More improvements to alltests.sh

--- platforms/Dos/alltests.sh
+++ platforms/Dos/alltests.sh
@@ -2,14 +2,15 @@
 printf "" > exit  # so dosbox will run 'external' command 'exit' correctly
 printf "" >ALLTESTS.LOG
 for test in `ls *.EXE`; do
-    bytes=`ls -l ${test} | awk '{print $6}'`
-    printf "Running ${test} (${bytes} bytes) inside DOSBox...\n"
+    kb=`ls -lsk ${test} | awk '{print $1}'`
+    printf "Running ${test} (${kb}k) inside DOSBox...\n"
     dosbox -conf ../platforms/Dos/dosbox-0.74.conf exit \
-    -c "echo *** ${test} (${bytes} bytes) *********************>>ALLTESTS.LOG" \
+    -c "echo *** ${test} (${kb}k) *****************************>>ALLTESTS.LOG" \
     -c "${test}>>ALLTESTS.LOG" \
     -noconsole -exit || exit 1
 done
 printf "\n"
-[ "`cat ALLTESTS.LOG`" ] || { printf "There were no tests to run!\n"; exit 1; }
+[ "`cat ALLTESTS.LOG`" ] || { printf "No tests to run!\n"; exit 1; }
+[ "`cat ALLTESTS.LOG|grep checks`" ] || { printf "No test output!\n"; exit 1; }
 cat ALLTESTS.LOG
-[ -z "`cat ALLTESTS.LOG|grep Failure`" ] || { printf "There were failed tests!\n"; exit 1; }
+[ -z "`cat ALLTESTS.LOG|grep Failure`" ] || { printf "Failed tests!\n"; exit 1; }
