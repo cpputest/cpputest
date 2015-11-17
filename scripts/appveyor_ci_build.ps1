dbb342a4bd5e81ea1fda3645ae6b81be83067228
Charles Nicholson <charles.nicholson@gmail.com>
2015-11-17 12:51:42 -0800

    keep Remove-PathFolder list alphabetically sorted

--- scripts/appveyor_ci_build.ps1
+++ scripts/appveyor_ci_build.ps1
@@ -120,9 +120,9 @@ if ($env:PlatformToolset -eq 'MinGW')
     Remove-PathFolder "C:\MinGW\bin"
     Remove-PathFolder "C:\Program Files\Git\bin"
     Remove-PathFolder "C:\Program Files\Git\cmd"
+    Remove-PathFolder "C:\Program Files\Git\usr\bin"
     Remove-PathFolder "C:\Program Files (x86)\Git\bin"
     Remove-PathFolder "C:\Program Files (x86)\Git\cmd"
-    Remove-PathFolder "C:\Program Files\Git\usr\bin"
 
     # Add mingw to the path
     Add-PathFolder $mingw_path
