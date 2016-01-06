4edaa1b84bba67bd4f279a3cedeb4f03d1240271
Bas Vodde <basv@odd-e.com>
2016-01-06 22:31:49 +0800

    Fixed compiler version number in comment

--- tests/UtestTest.cpp
+++ tests/UtestTest.cpp
@@ -191,7 +191,7 @@ TEST(UtestShell, TestDefaultCrashMethodInSeparateProcessTest)
     fixture.runAllTests();
     fixture.assertPrintContains("Failed in separate process - killed by signal");
 
-    /* Signal 11 usually happens, but with clang4.7 on Linux, it produced signal 4 */
+    /* Signal 11 usually happens, but with clang3.7 on Linux, it produced signal 4 */
     CHECK(fixture.getOutput().contains("signal 11") || fixture.getOutput().contains("signal 4"));
 }
 
