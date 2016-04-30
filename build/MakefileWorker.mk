b3470e7e4404ef7c0d08c597eef0fa327cbcff29
Bas Vodde <basv@odd-e.com>
2016-04-30 07:07:27 -0700

    Cleaned up configure.ac and added no long long to MakefileWorked

--- build/MakefileWorker.mk
+++ build/MakefileWorker.mk
@@ -176,7 +176,7 @@ endif
 
 # Default warnings
 ifndef CPPUTEST_WARNINGFLAGS
-	CPPUTEST_WARNINGFLAGS =  -Wall -Wextra -Werror -Wshadow -Wswitch-default -Wswitch-enum -Wconversion
+	CPPUTEST_WARNINGFLAGS =  -Wall -Wextra -Werror -Wshadow -Wswitch-default -Wswitch-enum -Wconversion -Wno-long-long
 ifeq ($(CPPUTEST_PEDANTIC_ERRORS), Y)
 	CPPUTEST_WARNINGFLAGS += -pedantic-errors
 endif
