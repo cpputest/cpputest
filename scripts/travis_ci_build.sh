3f6c1d98fcb91da07b5323497b86c9f881903113
Bas Vodde <basv@odd-e.com>
2016-09-17 15:23:52 +0800

    Fixed CMake gtest build?

--- scripts/travis_ci_build.sh
+++ scripts/travis_ci_build.sh
@@ -38,10 +38,14 @@ if [ "x$BUILD" = "xautotools_gtest" ]; then
 fi
 
 if [ "x$BUILD" = "xcmake_gtest" ]; then
-    wget https://googlemock.googlecode.com/files/gmock-1.6.0.zip
+	  wget https://github.com/google/googletest/archive/release-1.6.0.zip -O gtest-1.6.0.zip  && unzip gtest-1.6.0.zip;
+	  wget https://github.com/google/googlemock/archive/release-1.6.0.zip -O gmock-1.6.0.zip  && unzip gmock-1.6.0.zip;
+    unzip gtest-1.6.0.zip -d $TRAVIS_BUILD_DIR
     unzip gmock-1.6.0.zip -d $TRAVIS_BUILD_DIR
-    cd $TRAVIS_BUILD_DIR/gmock-1.6.0
-    ./configure && make
+    cd $TRAVIS_BUILD_DIR
+    mv googletest-release-1.6.0 googlemock-release-1.6.0/gtest
+    cd googlemock-release-1.6.0
+    autoreconf -i; ./configure CXXFLAGS=-DGTEST_USE_OWN_TR1_TUPLE=1 && make
     cd -
 
     export GMOCK_HOME=$TRAVIS_BUILD_DIR/gmock-1.6.0
