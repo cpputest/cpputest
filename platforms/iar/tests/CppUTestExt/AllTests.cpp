9416d2f371c574d8ddae22edc9679ee52b2e5b57
Daniel Gullberg <daniel.gullberg@mmm.com>
2016-11-11 08:39:26 +0100

    Adding CppUTestExt.ewp and CppUTestExtTest.ewp to the workspace
    Also adding an AllTests.cpp that will be special for the IAR environment. Right now it is just a copy of the original AllTests.cpp (Could this be solved by some #ifdefs instead in the original file?)

---
+++ platforms/iar/tests/CppUTestExt/AllTests.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2007, Michael Feathers, James Grenning and Bas Vodde
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE EARLIER MENTIONED AUTHORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <copyright holder> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "CppUTest/CommandLineTestRunner.h"
+#include "CppUTest/TestRegistry.h"
+#include "CppUTestExt/MemoryReporterPlugin.h"
+#include "CppUTestExt/MockSupportPlugin.h"
+
+#ifdef CPPUTEST_INCLUDE_GTEST_TESTS
+#include "CppUTestExt/GTestConvertor.h"
+#endif
+
+int main(int ac, const char** av)
+{
+#ifdef CPPUTEST_INCLUDE_GTEST_TESTS
+    GTestConvertor convertor;
+    convertor.addAllGTestToTestRegistry();
+#endif
+
+    MemoryReporterPlugin plugin;
+    MockSupportPlugin mockPlugin;
+    TestRegistry::getCurrentRegistry()->installPlugin(&plugin);
+    TestRegistry::getCurrentRegistry()->installPlugin(&mockPlugin);
+
+#ifndef GMOCK_RENAME_MAIN
+    return CommandLineTestRunner::RunAllTests(ac, av);
+#else
+    /* Don't have any memory leak detector when running the Google Test tests */
+
+    testing::GMOCK_FLAG(verbose) = testing::internal::kWarningVerbosity;
+
+    ConsoleTestOutput output;
+    CommandLineTestRunner runner(ac, av, &output, TestRegistry::getCurrentRegistry());
+    return runner.runAllTestsMain();
+#endif
+}
+
