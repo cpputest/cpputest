73c8a552b6d34a1eb5f44ecc502329fce3643b57
Asger Graarup Overby <ago@remoni.dk>
2019-06-06 10:23:08 +0200

    Use include "CppUTest/StandardCLibrary.h" to get memcpy.

--- src/CppUTestExt/MockNamedValue.cpp
+++ src/CppUTestExt/MockNamedValue.cpp
@@ -25,7 +25,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <cstring>
+#include "CppUTest/StandardCLibrary.h"
 
 #include "CppUTest/TestHarness.h"
 #include "CppUTestExt/MockNamedValue.h"
