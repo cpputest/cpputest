bbbf27b859456c39962660cbb8fa4be865d13d73
Vladimir Poddubnyi <vladimir.poddubnyi@nokia.com>
2019-12-06 18:46:45 +0300

    Add package to JUnit report file name
    If JUnit package is set, add it to the report file name.
    Slightly expand list of special characters not allowed in file names
    as package is provided via command line.

--- src/CppUTest/JUnitTestOutput.cpp
+++ src/CppUTest/JUnitTestOutput.cpp
@@ -151,8 +151,24 @@ void JUnitTestOutput::printCurrentTestStarted(const UtestShell& test)
 SimpleString JUnitTestOutput::createFileName(const SimpleString& group)
 {
     SimpleString fileName = "cpputest_";
+    if (!impl_->package_.isEmpty()) {
+        fileName += impl_->package_;
+        fileName += "_";
+    }
     fileName += group;
+
+    // special character list based on: https://en.wikipedia.org/wiki/Filename
     fileName.replace('/', '_');
+    fileName.replace('\\', '_');
+    fileName.replace('?', '_');
+    fileName.replace('%', '_');
+    fileName.replace('*', '_');
+    fileName.replace(':', '_');
+    fileName.replace('|', '_');
+    fileName.replace('"', '_');
+    fileName.replace('<', '_');
+    fileName.replace('>', '_');
+
     fileName += ".xml";
     return fileName;
 }
@@ -208,7 +224,7 @@ void JUnitTestOutput::writeTestCases()
         SimpleString buf = StringFromFormat(
                 "<testcase classname=\"%s%s%s\" name=\"%s\" assertions=\"%d\" time=\"%d.%03d\" file=\"%s\" line=\"%d\">\n",
                 impl_->package_.asCharString(),
-                impl_->package_.isEmpty() == true ? "" : ".",
+                impl_->package_.isEmpty() ? "" : ".",
                 impl_->results_.group_.asCharString(),
                 cur->name_.asCharString(),
                 cur->checkCount_ - impl_->results_.totalCheckCount_,
