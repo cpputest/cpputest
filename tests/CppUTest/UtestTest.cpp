af78518105df72d83366a5c30097a864c8ea19a8
Bas Vodde <basv@odd-e.com>
2020-04-16 14:33:47 +0800

    Moved the shuffle tests away

--- tests/CppUTest/UtestTest.cpp
+++ tests/CppUTest/UtestTest.cpp
@@ -382,3 +382,104 @@ TEST(CanHaveMemberVariablesInTestGroupThatAllocateMemoryWithoutCausingMemoryLeak
     dummy.allocateMoreMemory();
 }
 
+static int getZero()
+{
+    return 0;
+}
+
+static int getOne()
+{
+    return 1;
+}
+
+TEST_GROUP(UtestShellPointerArrayTest)
+{
+    UtestShell* test0;
+    UtestShell* test1;
+    UtestShell* test2;
+
+    void setup()
+    {
+        test0 = new IgnoredUtestShell();
+        test1 = new IgnoredUtestShell();
+        test2 = new IgnoredUtestShell();
+
+        test0->addTest(test1);
+        test1->addTest(test2);
+    }
+
+    void teardown()
+    {
+        delete test0;
+        delete test1;
+        delete test2;
+    }
+};
+
+
+TEST(UtestShellPointerArrayTest, empty)
+{
+    UtestShellPointerArray tests(NULLPTR);
+    tests.shuffle(0);
+    CHECK(NULL == tests.getFirstTest());
+}
+
+TEST(UtestShellPointerArrayTest, testsAreInOrder)
+{
+    UtestShellPointerArray tests(test0);
+    CHECK(tests.get(0) == test0);
+    CHECK(tests.get(1) == test1);
+    CHECK(tests.get(2) == test2);
+}
+
+TEST(UtestShellPointerArrayTest, relinkingTestsWillKeepThemTheSameWhenNothingWasDone)
+{
+    UtestShellPointerArray tests(test0);
+    tests.relinkTestsInOrder();
+    CHECK(tests.get(0) == test0);
+    CHECK(tests.get(1) == test1);
+    CHECK(tests.get(2) == test2);
+}
+
+
+TEST(UtestShellPointerArrayTest, firstTestisNotTheFirstTestWithSeed1234)
+{
+    UtestShellPointerArray tests(test0);
+    tests.shuffle(1234);
+    CHECK(tests.getFirstTest() != test0);
+}
+
+TEST(UtestShellPointerArrayTest, ShuffleListTestWithRandomAlwaysReturningZero)
+{
+    UT_PTR_SET(PlatformSpecificRand, getZero);
+
+    UtestShellPointerArray tests(test0);
+    tests.shuffle(3);
+    CHECK(tests.get(0) == test1);
+    CHECK(tests.get(1) == test2);
+    CHECK(tests.get(2) == test0);
+}
+
+// swaps with 4 mod 3 (1) then 4 mod 2 (0): 1, [2], [0] --> [1], [0], 2 --> 0, 1, 2
+TEST(UtestShellPointerArrayTest, ShuffleListTestWithRandomAlwaysReturningOne)
+{
+    UT_PTR_SET(PlatformSpecificRand, getOne);
+
+    UtestShellPointerArray tests(test0);
+    tests.shuffle(3);
+    CHECK(tests.get(0) == test0);
+    CHECK(tests.get(1) == test2);
+    CHECK(tests.get(2) == test1);
+}
+
+TEST(UtestShellPointerArrayTest, reverse)
+{
+    UT_PTR_SET(PlatformSpecificRand, getOne);
+
+    UtestShellPointerArray tests(test0);
+    tests.reverse();
+    CHECK(tests.get(0) == test2);
+    CHECK(tests.get(1) == test1);
+    CHECK(tests.get(2) == test0);
+}
+
