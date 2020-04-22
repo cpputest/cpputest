d895dcaf1292454e78f902918e33ceda2608d414
Bas Vodde <basv@odd-e.com>
2020-04-22 17:00:24 +0800

    Removed the console output of the dos box.

--- platforms/Dos/alltests.sh
+++ platforms/Dos/alltests.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+
+checkForCppUTestToolsEnvVariable() {
+	if [ -z "$CPPUTEST_HOME" ] ; then
+	   echo "CPPUTEST_HOME not set. You must set CPPUTEST_HOME to the top level CppUTest directory"
+	   exit 1
+	fi
+	if [ ! -d "$CPPUTEST_HOME" ] ; then
+	   echo "CPPUTEST_HOME not set to a directory.  You must set CPPUTEST_HOME to the top level CppUTest directory"
+	   exit 2
+	fi
+}
+
+checkForCppUTestToolsEnvVariable
+
 printf "" > exit  # so dosbox will run 'external' command 'exit' correctly
 printf "" >ALLTESTS.LOG
 for test in `ls *.EXE`; do
     kb=`ls -lsk ${test} | awk '{print $1}'`
     printf "Running ${test} (${kb}k) inside DOSBox...\n"
-    dosbox -conf ${CPPUTEST_HOME}/platforms/Dos/dosbox-0.74.conf exit \
+    dosbox -conf $CPPUTEST_HOME/platforms/Dos/dosbox-0.74.conf exit \
     -c "echo *** ${test} (${kb}k) *****************************>>ALLTESTS.LOG" \
     -c "${test}>>ALLTESTS.LOG" \
-    -noconsole -exit 2>dosbox_error.log || { printf "error running command!\n"; cat dosbox_error.log;  exit 1; }
+    -noconsole -exit 1>console_output 2>dosbox_error.log || { printf "error running command!\n"; cat dosbox_error.log;  exit 1; }
 done
 printf "\n"
 [ "`cat ALLTESTS.LOG`" ] || { printf "No tests to run!\n"; exit 1; }
