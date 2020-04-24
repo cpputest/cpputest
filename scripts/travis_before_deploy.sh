97cf6e9590425b1d6b29d777e7c8ae63b82a962e
Bas Vodde <basv@odd-e.com>
2020-04-24 15:06:31 +0800

    Retry to see if the deploy also pushes the tag

--- scripts/travis_before_deploy.sh
+++ scripts/travis_before_deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 set -ex
 
-openssl aes-256-cbc -K $encrypted_8ebb1ef83f64_key -iv $encrypted_8ebb1ef83f64_iv -in $TRAVIS_BUILD_DIR/github_deploy_key.enc -out github_deploy_key -d
-chmod 600 github_deploy_key
-eval $(ssh-agent -s)
-ssh-add github_deploy_key
+# openssl aes-256-cbc -K $encrypted_8ebb1ef83f64_key -iv $encrypted_8ebb1ef83f64_iv -in $TRAVIS_BUILD_DIR/github_deploy_key.enc -out github_deploy_key -d
+# chmod 600 github_deploy_key
+# eval $(ssh-agent -s)
+# ssh-add github_deploy_key
 
 git tag -f latest-passing-build -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
-git push -f git@github.com:cpputest/cpputest.git --tags
+# git push -f git@github.com:cpputest/cpputest.git --tags
