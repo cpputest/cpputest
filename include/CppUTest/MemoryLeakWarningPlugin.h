2aa9dc73638b03df78ca518508f389a13118ebc3
Bas Vodde <basv@odd-e.com>
2020-05-04 09:03:31 +0800

    IGNORE and EXPECT macros now don't crash when not having a memory leak plugin installed

--- include/CppUTest/MemoryLeakWarningPlugin.h
+++ include/CppUTest/MemoryLeakWarningPlugin.h
@@ -31,8 +31,8 @@
 #include "TestPlugin.h"
 #include "MemoryLeakDetectorNewMacros.h"
 
-#define IGNORE_ALL_LEAKS_IN_TEST() MemoryLeakWarningPlugin::getFirstPlugin()->ignoreAllLeaksInTest()
-#define EXPECT_N_LEAKS(n)          MemoryLeakWarningPlugin::getFirstPlugin()->expectLeaksInTest(n)
+#define IGNORE_ALL_LEAKS_IN_TEST() if (MemoryLeakWarningPlugin::getFirstPlugin()) MemoryLeakWarningPlugin::getFirstPlugin()->ignoreAllLeaksInTest()
+#define EXPECT_N_LEAKS(n)          if (MemoryLeakWarningPlugin::getFirstPlugin()) MemoryLeakWarningPlugin::getFirstPlugin()->expectLeaksInTest(n)
 
 extern void crash_on_allocation_number(unsigned alloc_number);
 
