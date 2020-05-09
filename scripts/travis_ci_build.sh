9285e937ae90651db969ab097e9c8778f7282168
Bas Vodde <basv@odd-e.com>
2020-05-09 13:20:26 +0800

    Trying travis visual C++ build

--- scripts/travis_ci_build.sh
+++ scripts/travis_ci_build.sh
@@ -147,9 +147,18 @@ if [ "x$BUILD" = "xautotools_dist" ]; then
     fi
 fi
 
+if [ "x$BUILD" = "xvc_windows" ]; then
+    BUILD_ARGS=("-DWERROR=ON")
+
+    if [ -n "$CPP_STD" ]; then
+        BUILD_ARGS+=("-DCMAKE_CXX_STANDARD=$CPP_STD")
+    fi
+
+    cmake --version
+    cmake "${BUILD_ARGS[@]}" ..
+fi
+
 if [ "x$BUILD" = "xcmake_windows" ]; then
-    pwd
-    ls
     choco install make
     BUILD_ARGS=("-DWERROR=ON")
 
