c8a662eca881c8c5c0f0092c1983d10f4511f303
Evgeny Litvinenko <evgeny.v.litvinenko@gmail.com>
2020-07-24 00:01:36 +0300

    Make function type and pointer to the function consistent

--- include/CppUTest/PlatformSpecificFunctions.h
+++ include/CppUTest/PlatformSpecificFunctions.h
@@ -33,8 +33,8 @@ TestOutput::WorkingEnvironment PlatformSpecificGetWorkingEnvironment();
 
 class TestPlugin;
 extern void (*PlatformSpecificRunTestInASeperateProcess)(UtestShell* shell, TestPlugin* plugin, TestResult* result);
-extern int (*PlatformSpecificFork)(void);
-extern int (*PlatformSpecificWaitPid)(int pid, int* status, int options);
+extern pid_t (*PlatformSpecificFork)(void);
+extern pid_t (*PlatformSpecificWaitPid)(int pid, int* status, int options);
 
 /* Platform specific interface we use in order to minimize dependencies with LibC.
  * This enables porting to different embedded platforms.
