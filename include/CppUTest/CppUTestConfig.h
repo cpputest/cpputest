885f6876f1ee6855e7386b94c3e23d727c42dc70
Chad Condon <chad@condon.tech>
2022-05-16 20:33:49 -0700

    Introduce CPPUTEST_FENV_DISABLED macro

--- include/CppUTest/CppUTestConfig.h
+++ include/CppUTest/CppUTestConfig.h
@@ -212,14 +212,21 @@
  * Handling of IEEE754 floating point exceptions via fenv.h
  * Predominantly works on non-Visual C++ compilers and Visual C++ 2008 and newer
  */
+#ifndef CPPUTEST_FENV_DISABLED
+ #if !CPPUTEST_USE_STD_C_LIB || \
+     (defined(_MSC_VER) && (_MSC_VER < 1800)) || \
+     defined(__APPLE__) || \
+     (defined(__ghs__) && defined(__ColdFire__)) || \
+     defined(__BCPLUSPLUS__)
+  #define CPPUTEST_FENV_DISABLED
+ #endif
+#endif
+
 #ifndef CPPUTEST_HAVE_FENV
- #if CPPUTEST_USE_STD_C_LIB && \
-     (!defined(_MSC_VER) || (_MSC_VER >= 1800)) && \
-     (!defined(__APPLE__)) && \
-     (!defined(__ghs__) || !defined(__ColdFire__)) && (!defined(__BCPLUSPLUS__))
-  #define CPPUTEST_HAVE_FENV 1
- #else
+ #ifdef CPPUTEST_FENV_DISABLED
   #define CPPUTEST_HAVE_FENV 0
+ #else
+  #define CPPUTEST_HAVE_FENV 1
  #endif
 #endif
 
