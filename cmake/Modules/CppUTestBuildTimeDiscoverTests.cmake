81efe5373f0c014e03635639ed2adcb97fa8286c
Chad Condon <chad@condon.tech>
2022-05-22 12:17:53 -0700

    Delete broken CMake install stuff

--- cmake/Modules/CppUTestBuildTimeDiscoverTests.cmake
+++ cmake/Modules/CppUTestBuildTimeDiscoverTests.cmake
@@ -1,13 +1,8 @@
+set(_CPPUTEST_BUILDTIME_DISCOVER_TESTS_DIR "${CMAKE_CURRENT_LIST_DIR}")
+
 # Create target to discover tests
 function (cpputest_buildtime_discover_tests EXECUTABLE)
-  # The path to the discover script depends on execution mode:
-  # - internal (building CppUTest it self).
-  # - imported (installed, imported, and executed by a client of the CppUTest lib)
-  if (PROJECT_NAME STREQUAL "CppUTest") # internal - (path is relative to source dir)
-    SET(DISCOVER_SCRIPT ${PROJECT_SOURCE_DIR}/cmake/Scripts/CppUTestBuildTimeDiscoverTests.cmake)
-  else (PROJECT_NAME STREQUAL "CppUTest") # Installed (path is relative to install directory)
-    SET(DISCOVER_SCRIPT ${CppUTest_DIR}/Scripts/CppUTestBuildTimeDiscoverTests.cmake)
-  endif (PROJECT_NAME STREQUAL "CppUTest")
+  set(DISCOVER_SCRIPT "${_CPPUTEST_BUILDTIME_DISCOVER_TESTS_DIR}/../Scripts/CppUTestBuildTimeDiscoverTests.cmake")
 
   add_custom_command (TARGET ${EXECUTABLE}
     POST_BUILD
