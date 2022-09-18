cd318ded7421855fd122fdea02536f59e0010898
Chad Condon <chad@condon.tech>
2022-09-18 13:15:09 -0700

    Use full libc

--- cmake/arm-none-eabi-gcc.toolchain.cmake
+++ cmake/arm-none-eabi-gcc.toolchain.cmake
@@ -5,4 +5,4 @@ set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 
 set(CMAKE_C_FLAGS_INIT "-mcpu=cortex-m4")
 set(CMAKE_CXX_FLAGS_INIT "-mcpu=cortex-m4")
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-specs=nano.specs -specs=rdimon.specs")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-specs=rdimon.specs")
