6ee9aba75afa413bcd0e8089da71e6a8644ac177
Chad Condon <chad@condon.tech>
2022-11-25 12:34:47 -0800

    Improve floating point exception support check
    Compiler checks are not a reliable indicator of support for floating
    point exception status flags.  Neither is checking that a program can
    _compile_ with `fenv.h`; linking can still fail (e.g. Open Watcom). This
    change checks `__STDC_IEC_559__` instead.  This macro "expands to 1 if
    IEC 60559 is supported".

--- include/CppUTest/CppUTestConfig.h
+++ include/CppUTest/CppUTestConfig.h
@@ -163,33 +163,15 @@
 #endif
 
 /*
- * Handling of IEEE754 floating point exceptions via fenv.h
+ * Handling of IEEE754 (IEC559) floating point exceptions via fenv.h
  * Predominantly works on non-Visual C++ compilers and Visual C++ 2008 and newer
  */
-#ifndef CPPUTEST_FENV_DISABLED
- #if !CPPUTEST_USE_STD_C_LIB || \
-     (defined(_MSC_VER) && (_MSC_VER < 1800)) || \
-     defined(__APPLE__) || \
-     (defined(__ghs__) && defined(__ColdFire__)) || \
-     defined(__BCPLUSPLUS__)
-  #define CPPUTEST_FENV_DISABLED
- #endif
-#endif
-
 #ifndef CPPUTEST_HAVE_FENV
- #ifdef CPPUTEST_FENV_DISABLED
-  #define CPPUTEST_HAVE_FENV 0
- #else
-  #define CPPUTEST_HAVE_FENV 1
+  #if __STDC_IEC_559__ && CPPUTEST_USE_STD_C_LIB
+    #define CPPUTEST_HAVE_FENV 1
+  #else
+    #define CPPUTEST_HAVE_FENV 0
  #endif
-#endif
-
-#if CPPUTEST_HAVE_FENV
-#if defined(__WATCOMC__) || defined(__ARMEL__) || defined(__m68k__)
-#define CPPUTEST_FENV_IS_WORKING_PROPERLY 0
-#else
-#define CPPUTEST_FENV_IS_WORKING_PROPERLY 1
-#endif
 #endif
 
 #ifdef __cplusplus
