9b858f0e4f86635a2407f612a99137de8c966fe6
Chad Condon <chad@condon.tech>
2022-12-08 22:53:40 -0800

    Cleanup macros

--- include/CppUTest/CppUTestConfig.h
+++ include/CppUTest/CppUTestConfig.h
@@ -134,24 +134,20 @@
  * To check whether it is on or off, we create a CppUTest define here.
 */
 #if defined(__has_feature)
-#if __has_feature(address_sanitizer)
-#define CPPUTEST_SANITIZE_ADDRESS 1
-#endif
-#endif
-
-#ifdef __SANITIZE_ADDRESS__
-#define CPPUTEST_SANITIZE_ADDRESS 1
+  #if __has_feature(address_sanitizer)
+    #define CPPUTEST_SANITIZE_ADDRESS 1
+  #endif
+#elif defined(__SANITIZE_ADDRESS__)
+  #define CPPUTEST_SANITIZE_ADDRESS 1
 #endif
 
 #ifndef CPPUTEST_SANITIZE_ADDRESS
-#define CPPUTEST_SANITIZE_ADDRESS 0
+  #define CPPUTEST_SANITIZE_ADDRESS 0
 #endif
 
 #if CPPUTEST_SANITIZE_ADDRESS
-  #define CPPUTEST_SANITIZE_ADDRESS 1
   #define CPPUTEST_DO_NOT_SANITIZE_ADDRESS __attribute__((no_sanitize_address))
 #else
-  #define CPPUTEST_SANITIZER_ADDRESS 0
   #define CPPUTEST_DO_NOT_SANITIZE_ADDRESS
 #endif
 
